3f67f8cd4924b3e6a45a04a8be55c72e71a0347a
Gene2430 <fabialaken15@gmail.com>
2022-11-18 17:44:18 +0800

    3D MAZE UPLOAD

---
+++ MY MAZES/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public CharacterController charController; // Publicly define our character controller
+
+    public bool canJump; // Enable this in the inspector if you want the player to jump
+
+    public float walkSpeed = 2.5f;
+    public float runSpeed = 5f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 1f;
+    public Transform groundCheck; // The transform of our "Ground Check" object. Used to determine if we are on the ground
+    public float groundDistance = 0.05f;
+    public LayerMask groundMask; // Set a field for our "Ground" layer
+    float speed; // Update-able moving speed
+
+    Vector3 velocity;
+    public bool grounded; // Defines is the player is touching the ground
+
+    private void Update() // Called once per frame
+    {
+        grounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask); // Checks if our player is touching an object with a "Ground" layer and sets the boolean accordingly
+
+        if (grounded && velocity.y < 0) // If the player is grounded
+        {
+            velocity.y = -2f; // Set the downward velocity to 2
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift)) // If the left shift button is being pressed
+        {
+            speed = runSpeed; // Set our speed to runSpeed
+        }
+        else
+        {
+            speed = walkSpeed;// Set our speed to walkSpeed
+        }
+
+        float x = Input.GetAxis("Horizontal"); // Define an X variable and set it to Unity's "Horizontal" axis
+        float z = Input.GetAxis("Vertical"); // Define a Y variable and set it to Unity's "Vertical" axis
+
+        Vector3 move = transform.right * x + transform.forward * z; // Define our move vector
+
+        charController.Move(move * speed * Time.deltaTime); // Move our character controller based on our set inputs
+
+        if (Input.GetButtonDown("Jump") && grounded && canJump) // Unity's default input for "Jump" is the space key
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity); // Set our jumping gravity
+        }
+
+        velocity.y += gravity * Time.deltaTime; // Set our regular gravity
+
+        charController.Move(velocity * Time.deltaTime); // Apply our gravity to our character controller
+    }
+}
---
+++ MY MAZES/Assets/Scripts/PlayerRotate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotate : MonoBehaviour
+{
+    public float rotationSensitivity = 750f; // Made public so it can be accessed by other scripts
+    public Transform playerBody; // The transform of our player body/mesh
+    float xRotation = 0f; // Define our x rotation variable
+    [SerializeField] private float clampDegreeUp = 70f; // How far the player can look up. Change to 90 for straight up
+    [SerializeField] private float clampDegreeDown = -90f; // How far the player can look down. Change if you feel like it
+
+    private void Start() // Called at game start
+    {
+        Cursor.visible = false; // Cursor will be hidden during play. Press Escape in Unity editor during play mode to change
+        Cursor.lockState = CursorLockMode.Locked; // Cursor will be locked at the center of the screen during play. Press Escape in Unity editor during play mode to change
+    }
+
+    private void Update() // Called once per frame
+    {
+        float mouseX = Input.GetAxis("Mouse X") * rotationSensitivity * Time.deltaTime; // Gets the horizontal mouse input
+        float mouseY = Input.GetAxis("Mouse Y") * rotationSensitivity * Time.deltaTime; // Gets the vertical mouse input
+
+        xRotation -= mouseY; // Our x rotation variable equals our vertical mouse input multiplied by -1
+        xRotation = Mathf.Clamp(xRotation, clampDegreeDown, clampDegreeUp); // Clamp our x rotation by a minimum and maximum value (How far the player can look up and down in degrees)
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f); // Implement the local rotation of the camera using our x rotation variable
+        playerBody.Rotate(Vector3.up * mouseX); // Rotate our player body/mesh on the Y axis based on the horizontal mouse input
+    }
+}
